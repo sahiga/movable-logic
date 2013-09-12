8238a803050ef013cb9572d49daeeda755d49208
Stephanie <stephanie@curiousminds.com>
2013-09-12 16:28:45 -0700

    Comments, delete button, additional cancel sort rules

--- logic.js
+++ logic.js
@@ -1,57 +1,101 @@
-
 $(document).ready(function() {
-	var elementClasses = [];
-    var classesInList = [];
-    var currentElement;
-    var prevElement;
     var originalParent;
     var originalIndex;
     var newIndex;
     var originalHTML;
+    var targetParent;
+    var checkCanceled = false;
+
+    function deleteParameter(list) {
+    	var deleteButton = list.children('.delete');
+
+    	// When the "x" button for a statement is clicked, delete the whole statement except for the dotted box
+    	deleteButton.click(function() {
+    		deleteButton.parent().children('li:not(.ui-state-dotted)').remove();
+	 		
+	 		// Add a dotted box to the statement if it does not already exist
+	 		if (list.children('.ui-state-dotted').length === 0) {
+	 			list.append('<li class="ui-state-dotted"></li>');
+	 		}
+    	});
+    }
 
   	function sortRules(event, ui) {
-	 	originalParent.children('.ui-state-dotted').remove();
+  		checkCanceled = false;
 
-	 	if (originalParent.children('li').length < 5 && (originalParent.attr('id') === 'statement1' || originalParent.attr('id') === 'statement2')) {
-	  		originalParent.append('<li class="ui-state-dotted"></li>');
-	 	}
+  		targetParent = ui.item.parent();
+	 	targetParent.children('.ui-state-dotted').remove();
 
-		if ((ui.item.attr('class') === ui.item.prev().attr('class')) ||
+	 	// If the to-list is a statement and (1) the from-list is a item list or (2) the to-list and from-list are identical
+	 	// Items can be rearranged within a single statement and dragged from a item list to a statement
+	 	// Items cannot be dragged from one statement to another or from a statement to a item list
+	 	if (targetParent.hasClass('fullStatement') && (originalParent.hasClass('piece') || originalParent.attr('id') === targetParent.attr('id'))) {
+	 		// Add a dotted box (implying that another item can be added) if the total statement length is shorter than five items
+	 		// If the total statement length is equal to five items, don't add another dotted box, but don't cancel the sort, either
+	 		if (targetParent.children('li:not(.delete):not(.ui-state-dotted)').length < 5 && !checkCanceled) {
+	 		  	targetParent.append('<li class="ui-state-dotted"></li>');
+	 		// Cancel the sort if the statement length has six or more items (including the current item)
+	 		} else if (targetParent.children('li:not(.delete):not(.ui-state-dotted)').length >= 6) {
+	 			checkCanceled = true;
+	 			$(this).sortable('cancel');
+	 		}
+	 	
+	 		// Add a delete button if the statement does not already have it
+	 		if (targetParent.children('.delete').length === 0) {
+	 		 	$('<li class="delete">x</li>').insertBefore(targetParent.children('li:first-child'));
+	 		}
+
+	 		// Prevent like items from being placed next to each other
+	 		// i.e., operators should never be next to operators, and parameters should never be next to parameters
+	 		// Check if the item has the same class as the item before it or the item after it
+	 		// or if the item is an operator and (1) the previous item is not a parameter (e.g., operator or undefined),
+	 		// (2) the next item is an operator, and (3) the next item is undefined
+	 		// This prevents operators from being placed next to each other, at the beginning or end of a statement,
+	 		// but allows them to appear before a dotted box
+	 		if ((ui.item.attr('class') === ui.item.prev().attr('class')) ||
 			(ui.item.attr('class') === ui.item.next().attr('class')) ||
-			(ui.item.hasClass('operator') && (!ui.item.prev().hasClass('noun') || ui.item.next().hasClass('operator') || ui.item.next().attr('class') === undefined))) {
+			(ui.item.hasClass('operator') && !ui.item.prev().hasClass('noun') && !ui.item.next().hasClass('operator') && ui.item.next().attr('class') === undefined)) {
 
-			if (originalParent.attr('id') === 'operators' || originalParent.attr('id') === 'parameters') {
-				$(this).sortable('cancel');
-			} else {
+ 				// Grab the new target index of the item (after the sort ends)
 				newIndex = ui.item.index();
 
+				// Swap the current item with the item at its target index
+				// if the target item is an even number away from the current item,
+				// so that only like items are swapped with each other
 				if (Math.abs(originalIndex - newIndex) % 2 === 0) {
 			 		ui.item.next().insertAfter(originalParent.children('li').eq(originalIndex));
+			 	// Otherwise, cancel the sort
 				} else {
+ 					checkCanceled = true;
 					$(this).sortable('cancel');
 				}
 
-			}
-
-		} else {
-			if (ui.item.hasClass('noun')) {
-				ui.item.html('<input type="text">');
-				ui.item.css('padding', '5px');
-			}
-
-			if (originalHTML === 'Location') {
-				ui.item.children('input').attr({
-					'id': 'locationInput',
-					'placeholder': 'Enter your city'
-				});
-				locationSearch();
+			// Otherwise, if the sort arrangement is allowed, continue with the sort
 			} else {
-				ui.item.children('input').attr('placeholder', originalHTML);
+				if (ui.item.hasClass('noun')) {
+					ui.item.html('<input type="text" class="search-input">');
+					ui.item.css('padding', '5px');
+				}
+
+				if (originalHTML === 'Location') {
+					ui.item.children('input').attr({
+						'id': 'locationInput',
+						'placeholder': 'Enter your city'
+					});
+					locationSearch();
+				} else {
+					ui.item.children('input').attr('placeholder', originalHTML);
+				}
+
+				originalParent.append('<li class="' + ui.item.attr('class') + '">' + originalHTML + '</li>');
+				deleteParameter(targetParent);
 			}
 
-		originalParent.append('<li class="' + ui.item.attr('class') + '">' + originalHTML + '</li>');
-		}
-
+		// Prevent items from being dragged from statements back into item lists
+	 	} else {
+	 		checkCanceled = true;
+	 		$(this).sortable('cancel');
+	 	}
   	}
 
   	function locationSearch() {
@@ -64,11 +108,10 @@ $(document).ready(function() {
   		var autocomplete = new google.maps.places.Autocomplete(input, cities);
   	}
 
-
   $(function() {
-
     $('#statement1, #statement2, #operators, #parameters').sortable({
     	connectWith: '.connectLists',
+    	cancel: '.delete, .ui-state-dotted',
     	start: function(event, ui) {
     		originalParent = ui.item.parent();
 	    	originalIndex = ui.item.index();
