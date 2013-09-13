62a180a2939c4491633a6c67479604b316ed4dc6
Stephanie <stephanie@curiousminds.com>
2013-09-13 12:14:44 -0700

    Fixed in-statement swapping issues

--- logic.js
+++ logic.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     var newIndex;
     var originalHTML;
     var targetParent;
-    var checkCanceled = false;
 
     function deleteParameter(list) {
     	var deleteButton = list.children('.delete');
@@ -20,23 +19,26 @@ $(document).ready(function() {
     	});
     }
 
-  	function sortRules(event, ui) {
-  		checkCanceled = false;
+    function checkCanceled(list) {
+    	if (list.length < 5 && list.hasClass('fullStatement') && list.children('.ui-state-dotted').length === 0) {
+	 		targetParent.append('<li class="ui-state-dotted"></li>');
+    	}
+    }
 
+  	function sortRules(event, ui) {
   		targetParent = ui.item.parent();
 	 	targetParent.children('.ui-state-dotted').remove();
 
 	 	// If the to-list is a statement and (1) the from-list is a item list or (2) the to-list and from-list are identical
 	 	// Items can be rearranged within a single statement and dragged from a item list to a statement
 	 	// Items cannot be dragged from one statement to another or from a statement to a item list
-	 	if (targetParent.hasClass('fullStatement') && (originalParent.hasClass('piece') || originalParent.attr('id') === targetParent.attr('id'))) {
+	 	if (targetParent.hasClass('fullStatement') && (originalParent.hasClass('piece'))) {
 	 		// Add a dotted box (implying that another item can be added) if the total statement length is shorter than five items
 	 		// If the total statement length is equal to five items, don't add another dotted box, but don't cancel the sort, either
-	 		if (targetParent.children('li:not(.delete):not(.ui-state-dotted)').length < 5 && !checkCanceled) {
+	 		if (targetParent.children('li:not(.delete):not(.ui-state-dotted)').length < 5) {
 	 		  	targetParent.append('<li class="ui-state-dotted"></li>');
 	 		// Cancel the sort if the statement length has six or more items (including the current item)
 	 		} else if (targetParent.children('li:not(.delete):not(.ui-state-dotted)').length >= 6) {
-	 			checkCanceled = true;
 	 			$(this).sortable('cancel');
 	 		}
 	 	
@@ -53,26 +55,16 @@ $(document).ready(function() {
 	 		// This prevents operators from being placed next to each other, at the beginning or end of a statement,
 	 		// but allows them to appear before a dotted box
 	 		if ((ui.item.attr('class') === ui.item.prev().attr('class')) ||
-			(ui.item.attr('class') === ui.item.next().attr('class')) ||
-			(ui.item.hasClass('operator') && !ui.item.prev().hasClass('noun') && !ui.item.next().hasClass('operator') && ui.item.next().attr('class') === undefined)) {
+				(ui.item.attr('class') === ui.item.next().attr('class')) ||
+				(ui.item.hasClass('operator') && (!ui.item.prev().hasClass('parameter') || ui.item.next().hasClass('operator') || ui.item.next().attr('class') === undefined)) ||
+				ui.item.prev().hasClass('ui-state-dotted')) {
 
- 				// Grab the new target index of the item (after the sort ends)
-				newIndex = ui.item.index();
-
-				// Swap the current item with the item at its target index
-				// if the target item is an even number away from the current item,
-				// so that only like items are swapped with each other
-				if (Math.abs(originalIndex - newIndex) % 2 === 0) {
-			 		ui.item.next().insertAfter(originalParent.children('li').eq(originalIndex));
-			 	// Otherwise, cancel the sort
-				} else {
- 					checkCanceled = true;
-					$(this).sortable('cancel');
-				}
+				$(this).sortable('cancel');
+				checkCanceled(targetParent);
 
 			// Otherwise, if the sort arrangement is allowed, continue with the sort
 			} else {
-				if (ui.item.hasClass('noun')) {
+				if (ui.item.hasClass('parameter')) {
 					ui.item.html('<input type="text" class="search-input">');
 					ui.item.css('padding', '5px');
 				}
@@ -90,11 +82,24 @@ $(document).ready(function() {
 				originalParent.append('<li class="' + ui.item.attr('class') + '">' + originalHTML + '</li>');
 				deleteParameter(targetParent);
 			}
+		} else if (targetParent.attr('id') === originalParent.attr('id')) {
+ 				// Grab the new target index of the item (after the sort ends)
+				newIndex = ui.item.index();
+
+				// Swap the current item with the item at its target index
+				// if the target item is an even number away from the current item,
+				// so that only like items are swapped with each other
+				if (Math.abs(originalIndex - newIndex) % 2 === 0 && (ui.item.hasClass('operator') && !ui.item.prev().hasClass('operator'))) {
+			 		ui.item.next().insertAfter(originalParent.children('li').eq(originalIndex));
+			 	} else {
+			 		$(this).sortable('cancel');
+					checkCanceled(targetParent);
+			 	}
 
 		// Prevent items from being dragged from statements back into item lists
 	 	} else {
-	 		checkCanceled = true;
 	 		$(this).sortable('cancel');
+			checkCanceled(targetParent);
 	 	}
   	}
 
